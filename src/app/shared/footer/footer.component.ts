52b02534af282713d8377d05e6fe8da11b5b273d
Rahul Anand <rahul.anand@sakhatech.com>
2017-08-29 12:49:15 +0530

    Initial

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, HostBinding } from '@angular/core';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'footer-cmp',
+    templateUrl: 'footer.component.html'
+})
+
+export class FooterComponent{
+    test : Date = new Date();
+}
---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, HostBinding } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'menu-cmp',
+    templateUrl: 'menu.component.html'
+})
+
+export class MenuComponent{
+    flagActiveDashboard: string;
+    flagActiveOrganisation: string;
+    flagActiveSettings: string;
+    currentpath: string;
+
+    constructor(
+        private route: ActivatedRoute) { }
+
+    ngOnInit() {
+    	this.route.url.subscribe((params: Params) => {
+    		if(params.length){
+		        this.currentpath =  params[0].path || '/';
+		        this.activateMenuClass(this.currentpath);
+    		}else{
+    			this.flagActiveDashboard = 'active';
+    		}
+	    });
+    }
+
+    activateMenuClass(urlparam: string) {
+    	switch(urlparam){
+       		case 'dashboard':
+       			this.flagActiveDashboard = 'active';
+       		break;
+       		case 'organisation':
+       			this.flagActiveOrganisation = 'active';
+       		break;
+       		case 'settings':
+       			this.flagActiveSettings = 'active';
+       		break;
+       		default:
+       			this.flagActiveDashboard = 'active';
+       		break;
+       }
+    }
+}
